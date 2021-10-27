7e144d33adad7d2a4128737949d29eebf92e20f0
htrie <46622503+htrie@users.noreply.github.com>
2021-10-27 23:52:09 +1300

    Add files via upload

---
+++ config.el
@@ -0,0 +1,6 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq doom-font (font-spec :family "PragmataPro Mono Liga" :size 10 :weight 'regular))
+(setq doom-theme 'doom-gruvbox)
+(setq display-line-numbers-type 'relative)
+(setq create-lockfiles nil)
